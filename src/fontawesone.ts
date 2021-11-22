26aa46ca7fe13c29a82706edbcd8015322f42636
AnnaKoliada <anacirec@gmail.com>
2021-11-22 09:54:16 +0300

    fix errors

--- src/fontawesone.ts
+++ src/fontawesone.ts
@@ -1,20 +1,9 @@
-import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
-import { icon } from '@fortawesome/fontawesome-svg-core'
+import { dom, config, icon } from '@fortawesome/fontawesome-svg-core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 config.autoAddCss = false;
 config.keepOriginalSource = false;
 config.autoReplaceSvg = true;
 config.observeMutations = true;
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-export const icon1 = icon(faUser).html
-// this is the 100% working way (deep imports)
-// import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
-// import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
-// import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook';
-// import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube';
-
-// // this is the treeshaking way (better, but read about it below)
-// import { faUser, faHome } from '@fortawesome/free-solid-svg-icons';
-// import { faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
-
-// library.add(faUser, faHome, faFacebook, faYoutube);
+export const icon1 = icon(faUser).html;
 dom.watch();
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,27 @@
-import './fontawesone';
 import './styles.scss';
 import { icon1 } from './fontawesone';
 
 const API_KEY = 'dedb83e68e64484ba04011e773888fb9';
-
-const root: HTMLDivElement | null = document.querySelector('.root')
-const form: HTMLFormElement = document.createElement('form')
-
+const root: HTMLDivElement | null = document.querySelector('.root');
+const form: HTMLFormElement = document.createElement('form');
 form.innerHTML = `<input class='paralellogramm input' type="text" required placeholder="Ваш номер...">
 <button class='paralellogramm button' type='submit'> <p class='content'> ${icon1} Заказать</p>
-</button>`
+</button>`;
 if (root) {
-  root.appendChild(form)
+  root.appendChild(form);
 }
-form.addEventListener('submit', (event: Event) => sendRequest(event))
 
 async function sendRequest(event: Event) {
-  event.preventDefault()
+  event.preventDefault();
   const input: HTMLInputElement | null = document.querySelector('.input');
 
-  var invocation = new XMLHttpRequest();
+  const invocation = new XMLHttpRequest();
   if (invocation) {
-    let response = await fetch(`https://newsapi.org/v2/everything?q=${input?.value}&apiKey=${API_KEY}`);
-
-    let result = await response.ok;
-    if(result){
-      alert("Ваш запрос успешно отправлен");
-    } else alert("Ваш запрос не удалось отправить");
-
-
+    const response = await fetch(`https://newsapi.org/v2/everything?q=${input?.value}&apiKey=${API_KEY}`);
+    const result = await response.ok;
+    if (result) {
+      alert('Ваш запрос успешно отправлен');
+    } else alert('Ваш запрос не удалось отправить');
   }
-
-
 }
-
-// https://calm-spire-07097.herokuapp.com/api/categories/
-// https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits
+form.addEventListener('submit', (event: Event) => sendRequest(event));
